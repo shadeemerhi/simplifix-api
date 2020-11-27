82e6a9937104a638a6197ee873763883da400db0
Tom Adam <tom.adam21@gmail.com>
2020-11-27 14:52:42 -0800

    minor change to orders route

--- src/routes/orders.js
+++ src/routes/orders.js
@@ -2,26 +2,28 @@ const router = require("express").Router();
 
 module.exports = (helpers) => {
   router.get("/orders", (req, res) => {
-    helpers.getOrders()
-    .then(data => res.send(data))
-    .catch(err => res.status(400).send(err));
+    helpers
+      .getOrders()
+      .then((data) => res.send(data))
+      .catch((err) => res.status(400).send(err));
   });
   router.put("/orders", (req, res) => {
     const order = req.body;
-    helpers.createOrder(order)
-    .then(data => {
-      setTimeout(() => {
-        res.send(data)
-      }, 5000);     
-    })
-    .catch(err => res.status(400).send(err));
+    helpers
+      .createOrder(order)
+      .then((data) => {
+        setTimeout(() => {
+          res.send(data);
+        }, 5000);
+      })
+      .catch((err) => res.status(400).send(err));
   });
 
   router.patch("/orders/:id", (req, res) => {
-    
     const order = req.body;
-    helpers.changeOrder(order)
-    .then(data => res.send(data));
+
+    console.log("order :", order);
+    helpers.changeOrder(order).then((data) => res.send(data));
   });
   return router;
 };
