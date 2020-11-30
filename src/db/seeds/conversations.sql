8722d75595db2069a39a7e0b15e2343a0afcc726
Shadee Merhi <shadeemerhi@Shadees-MacBook-Pro-2.local>
2020-11-30 11:45:05 -0700

    added 'clicked' boolean to seed convos

--- src/db/seeds/conversations.sql
+++ src/db/seeds/conversations.sql
@@ -1,4 +1,4 @@
-INSERT INTO conversations (client_id, client_first, client_last, contractor_id, contractor_first, contractor_last)
+INSERT INTO conversations (client_id, client_first, client_last, contractor_id, contractor_first, contractor_last, clicked)
 VALUES
-(1, 'Shadee', 'Merhi', 2, 'Doug', 'Person'),
-(2, 'Doug', 'Person', 3, 'Jason', 'Dude');
+(1, 'Shadee', 'Merhi', 2, 'Doug', 'Person', true),
+(2, 'Doug', 'Person', 3, 'Jason', 'Dude', true);
