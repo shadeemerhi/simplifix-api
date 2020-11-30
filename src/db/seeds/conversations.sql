e07a47d67bd5e2044b008b34596c4efda2965769
Shadee Merhi <shadeemerhi@Shadees-MacBook-Pro-2.local>
2020-11-30 11:40:34 -0700

    removed empty seed convo

--- src/db/seeds/conversations.sql
+++ src/db/seeds/conversations.sql
@@ -1,5 +1,4 @@
 INSERT INTO conversations (client_id, client_first, client_last, contractor_id, contractor_first, contractor_last)
 VALUES
 (1, 'Shadee', 'Merhi', 2, 'Doug', 'Person'),
-(2, 'Doug', 'Person', 3, 'Jason', 'Dude'),
-(3, 'Jason', 'Dude', 1, 'Shadee', 'Merhi');
+(2, 'Doug', 'Person', 3, 'Jason', 'Dude');
